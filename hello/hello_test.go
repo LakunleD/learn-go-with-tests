da9ed914a5a3d458795321d507c88046abd5d500
Olakunle <OlakunleDosunmu@gmail.com>
2020-02-03 13:20:34 +0100

    passing name dynamically

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello()
-	want := "Hello, world"
+	got := Hello("kunle")
+	want := "Hello, kunle"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
