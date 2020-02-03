b6da39b7e24864a219810de5e3f14b7c0dbc4236
Olakunle <OlakunleDosunmu@gmail.com>
2020-02-03 13:51:01 +0100

    refactoring the hello function to accommodate languages

--- hello/hello_test.go
+++ hello/hello_test.go
@@ -12,15 +12,41 @@ func TestHello(t *testing.T) {
 	}
 
 	t.Run("saying hello to people", func(t *testing.T) {
-		got := Hello("kunle")
+		got := Hello("kunle", "")
 		want := "Hello, kunle"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T) {
-		got := Hello("")
-		want := "Hello, world"
+		got := Hello("", "")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in spanish", func(t *testing.T) {
+		got := Hello("Rafael", "Spanish")
+		want := "Hola, Rafael"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Hola, World' when an empty string is supplied in Spanish", func(t *testing.T) {
+		got := Hello("", "Spanish")
+		want := "Hola, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in french", func(t *testing.T) {
+		got := Hello("Mendy", "French")
+		want := "Bonjour, Mendy"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say 'Bonjour, World' when an empty string is supplied in French", func(t *testing.T) {
+		got := Hello("", "French")
+		want := "Bonjour, World"
 
 		assertCorrectMessage(t, got, want)
 	})
