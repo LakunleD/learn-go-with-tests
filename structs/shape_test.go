199fb7dfe7731b2e3658068ed893ced6fc3d6093
Olakunle <OlakunleDosunmu@gmail.com>
2020-02-04 12:46:07 +0100

    using interfaces and methods

---
+++ structs/shape_test.go
@@ -0,0 +1,35 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 3.0}
+	got := Perimeter(rectangle)
+	want := 26.0
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+
+	checkArea := func(t *testing.T, shape Shape, want float64) {
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %.2f want %.2f", got, want)
+		}
+	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{10.0, 3.0}
+		want := 30.0
+		checkArea(t, rectangle, want)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		want := 314.1592653589793
+		checkArea(t, circle, want)
+	})
+}
