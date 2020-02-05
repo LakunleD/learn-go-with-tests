59d3a68f5f30870b5fc5d595695223c18e2d9587
Olakunle <OlakunleDosunmu@gmail.com>
2020-02-05 11:55:50 +0100

    fixing the insufficient funds bug and refactoring the codebase

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -11,6 +11,24 @@ func TestWallet(t *testing.T) {
 		}
 	}
 
+	assertNoError := func(t *testing.T, got error) {
+		t.Helper()
+		if got != nil {
+			t.Fatal("got an error but didn't want one")
+		}
+	}
+
+	assertError := func(t *testing.T, got error, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+
+		if got != want {
+			t.Errorf("got %q, want %q", got, want)
+		}
+	}
+
 	t.Run("Deposit", func(t *testing.T) {
 		wallet := Wallet{}
 		wallet.Deposit(Bitcoin(10))
@@ -20,7 +38,22 @@ func TestWallet(t *testing.T) {
 
 	t.Run("Withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: Bitcoin(10)}
-		wallet.Withdraw(Bitcoin(10))
+		err := wallet.Withdraw(Bitcoin(10))
 		balance(t, wallet, Bitcoin(0))
+
+		assertNoError(t, err)
+	})
+
+	t.Run("Insufficient funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		balance(t, wallet, startingBalance)
+		assertError(t, err, ErrInsufficientFunds)
+
+		if err == nil {
+			t.Error("wanted an error but didn't get one")
+		}
 	})
 }
