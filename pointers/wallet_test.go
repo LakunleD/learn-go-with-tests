ea750bd156621cabdd5134badf91302a8bd2b8a5
Olakunle <OlakunleDosunmu@gmail.com>
2020-02-05 11:29:34 +0100

    changed the wallet to a bitcoin wallet and withdrawing

--- pointers/wallet_test.go
+++ pointers/wallet_test.go
@@ -3,12 +3,24 @@ package pointers
 import "testing"
 
 func TestWallet(t *testing.T) {
-	wallet := Wallet{}
-	wallet.Deposit(10)
-	got := wallet.Balance()
-	want := 10
-
-	if got != want {
-		t.Errorf("got %d want %d", got, want)
+	balance := func(t *testing.T, w Wallet, want Bitcoin) {
+		t.Helper()
+		got := w.Balance()
+		if got != want {
+			t.Errorf("got %d want %d", got, want)
+		}
 	}
+
+	t.Run("Deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		balance(t, wallet, Bitcoin(10))
+
+	})
+
+	t.Run("Withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(10)}
+		wallet.Withdraw(Bitcoin(10))
+		balance(t, wallet, Bitcoin(0))
+	})
 }
